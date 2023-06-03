5fb254c70d791cc292406842a659342032cc9177
Roshan Majhi <roshanmajbi39@gmail.com>
2023-06-03 14:19:55 +0545

    This is challenging commit

---
+++ index.js
@@ -0,0 +1,19 @@
+//scrolltotop function
+
+$(document).ready(function(){
+    $(window).scroll(function(){
+        if($(this).scrollTop()>100){
+            $('#top').fadeIn();
+        }
+        else{
+            $('#top').fadeOut();
+        }
+    })
+    $('#top').click(function(){
+        $('html, body').animate({scrollTop:0}, 1500);
+        return false;
+
+    })
+ })
+
+ 
